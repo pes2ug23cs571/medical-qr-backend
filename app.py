7fae908070ca4adf69f3f590273f54db4b3a9938
pes2ug23cs571 <pes2ug23cs571@pesu.pes.edu>
2026-02-05 12:27:40 +0530

    Update database connection to use environment variables
    Refactor database connection to use environment variables for configuration.

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
+import os
 from flask import Flask, jsonify
 import mysql.connector
 
 app = Flask(__name__)
 
-# Database connection
-
 db = mysql.connector.connect(
-    host="mysql.railway.internal",
-    user="root",
-    password="IbYnOfdheNcHeXECWJbejuPYoDeyqNkP",
-    database="railway",
-    port=int("3306")
+    host=os.environ.get("MYSQLHOST"),
+    user=os.environ.get("MYSQLUSER"),
+    password=os.environ.get("MYSQLPASSWORD"),
+    database=os.environ.get("MYSQLDATABASE"),
+    port=int(os.environ.get("MYSQLPORT"))
 )
-
 
 @app.route("/qr/<qr_id>")
 def get_critical_info(qr_id):
     cursor = db.cursor(dictionary=True)
-    query = "SELECT name, blood_group, allergies, chronic_conditions, critical_medicines, emergency_contact, last_updated FROM critical_info WHERE qr_id = %s"
+    query = """
+    SELECT name, blood_group, allergies,
+           chronic_conditions, critical_medicines,
+           emergency_contact, last_updated
+    FROM critical_info
+    WHERE qr_id = %s
+    """
     cursor.execute(query, (qr_id,))
     result = cursor.fetchone()
     cursor.close()
@@ -28,4 +32,4 @@ def get_critical_info(qr_id):
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
